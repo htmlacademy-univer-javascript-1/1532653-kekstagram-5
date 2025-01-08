04b1b519f62c6ed1c54fbf629fb0bc4f275b7590
venfar <farlenkovveniamin@gmail.com>
2025-01-08 21:14:17 +0500

    Финальные правки

--- js/util.js
+++ js/util.js
@@ -1,14 +1,53 @@
 const getRandomInteger = (a, b) => {
-  let lower = Math.ceil(Math.min(a, b));
-  let upper = Math.floor(Math.max(a, b));
-  let result = Math.random() * (upper - lower + 1) + lower;
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
-
 const isEnterKey = (evt) => evt.key === 'Enter';
+const showErrorAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.id = 'error-alert';
+  alertContainer.style.zIndex = '10';
+  alertContainer.style.position = 'fixed';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 20px';
+  alertContainer.style.fontFamily = '"Open Sans", "Arial", sans-serif';
+  alertContainer.style.fontWeight = '700';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.color = '#ffe753';
+  alertContainer.style.backgroundColor = '#3c3614';
 
-export {getRandomInteger, getRandomArrayElement, isEscapeKey, isEnterKey};
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, 1000);
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const setRandomArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = getRandomInteger(0, i);
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export {getRandomInteger, getRandomArrayElement, isEscapeKey, isEnterKey, showErrorAlert, debounce, setRandomArray};
